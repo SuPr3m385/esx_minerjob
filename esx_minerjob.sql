6acbec16187f8f7fca7dfe3dcc9a1878de4ffc73
NeQYT <46975849+NeQYT@users.noreply.github.com>
2019-08-20 03:49:17 +0200

    Update esx_minerjob.sql

--- esx_minerjob.sql
+++ esx_minerjob.sql
@@ -6,9 +6,9 @@
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-	('miner', 'Miner'),
+('miner', 'Miner'),
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('miner', 0, 'employee', 'Employee', 0, '{}', '{}')
+('miner', 0, 'employee', 'Employee', 0, '{}', '{}')
 ,
