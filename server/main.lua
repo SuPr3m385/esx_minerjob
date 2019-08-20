fd4862784868a21eb73b665ea6c187b0ff0f47ac
NeQYT <46975849+NeQYT@users.noreply.github.com>
2019-08-20 22:20:05 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,8 +6,8 @@ AddEventHandler("esx_miner:givestone", function(item, count)
     local _source = source
     local xPlayer  = ESX.GetPlayerFromId(_source)
         if xPlayer ~= nil then
-            if xPlayer.getInventoryItem('kamienie').count < 40 then
-                xPlayer.addInventoryItem('kamienie', 5)
+            if xPlayer.getInventoryItem('stones').count < 40 then
+                xPlayer.addInventoryItem('stones', 5)
                 TriggerClientEvent('esx:showNotification', source, 'You received ~b~stones.')
             end
         end
@@ -19,11 +19,11 @@ AddEventHandler("esx_miner:washing", function(item, count)
     local _source = source
     local xPlayer  = ESX.GetPlayerFromId(_source)
         if xPlayer ~= nil then
-            if xPlayer.getInventoryItem('kamienie').count > 9 then
+            if xPlayer.getInventoryItem('stones').count > 9 then
                 TriggerClientEvent("esx_miner:washing", source)
                 Citizen.Wait(15900)
-                xPlayer.addInventoryItem('umytekamienie', 10)
-            elseif xPlayer.getInventoryItem('kamienie').count < 10 then
+                xPlayer.addInventoryItem('washedstones', 10)
+            elseif xPlayer.getInventoryItem('stones').count < 10 then
                 TriggerClientEvent('esx:showNotification', source, 'You do not have ~b~stones.')
             end
         end
@@ -35,27 +35,27 @@ AddEventHandler("esx_miner:remelting", function(item, count)
     local xPlayer  = ESX.GetPlayerFromId(_source)
     local randomChance = math.random(1, 100)
         if xPlayer ~= nil then
-            if xPlayer.getInventoryItem('umytekamienie').count > 9 then
+            if xPlayer.getInventoryItem('washedstones').count > 9 then
                 TriggerClientEvent("esx_miner:remelting", source)
                 Citizen.Wait(15900)
                 if randomChance < 10 then
                     xPlayer.addInventoryItem("diamond", 1)
-                    xPlayer.removeInventoryItem("umytekamienie", 10)
+                    xPlayer.removeInventoryItem("washedstones", 10)
                     TriggerClientEvent('esx:showNotification', _source, 'You get ~b~1 diamond ~w~for 10 washed stones.')
                 elseif randomChance > 9 and randomChance < 25 then
                     xPlayer.addInventoryItem("gold", 5)
-                    xPlayer.removeInventoryItem("umytekamienie", 10)
+                    xPlayer.removeInventoryItem("washedstones", 10)
                     TriggerClientEvent('esx:showNotification', _source, 'You get ~y~5 gold ~w~for 10 washed stones.')
                 elseif randomChance > 24 and randomChance < 50 then
                     xPlayer.addInventoryItem("iron", 10)
-                    xPlayer.removeInventoryItem("umytekamienie", 10)
+                    xPlayer.removeInventoryItem("washedstones", 10)
                     TriggerClientEvent('esx:showNotification', _source, 'You got ~w~10 iron for 10 washed stones.')
                 elseif randomChance > 49 then
                     xPlayer.addInventoryItem("copper", 20)
-                    xPlayer.removeInventoryItem("umytekamienie", 10)
+                    xPlayer.removeInventoryItem("washedstones", 10)
                     TriggerClientEvent('esx:showNotification', _source, 'You got ~o~20 copper ~w~for 10 washed stones.')
                 end
-            elseif xPlayer.getInventoryItem('kamienie').count < 10 then
+            elseif xPlayer.getInventoryItem('stones').count < 10 then
                 TriggerClientEvent('esx:showNotification', source, 'You do not have ~b~stones.')
             end
         end
