b65d0593ef02cde854fa1059e74c4234938c2688
Sandy <35917080+SuPr3m385@users.noreply.github.com>
2019-09-05 17:06:39 +0200

    Update
    Traduction en français + Correction de la ligne copper

--- esx_minerjob.sql
+++ esx_minerjob.sql
@@ -1,16 +1,16 @@
 INSERT INTO `jobs` (name, label) VALUES
-	('miner', 'Miner')
+	('miner', 'Mineur')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('miner', 0, 'employee', 'Employee', 0, '{}', '{}')
+	('miner', 0, 'employee', 'Ouvrier', 0, '{}', '{}')
 ;
 
 INSERT INTO `items` (`name`, `label`, `limit`) VALUES
-	('stones', 'Stones', 40),
-	('washedstones', 'Washed stones', 40),
-	('diamond', 'Diamond', 100),
-	('gold', 'Gold', 100),
-	('iron', 'Iron', 100),
-	('copper', 'Diamond', 100)
+	('stone', 'Minerai', 40),
+	('washedstone', 'Minerai Nettoyé', 40),
+	('diamond', 'Diamant', 100),
+	('gold', 'Or', 100),
+	('iron', 'Fer', 100),
+	('copper', 'Cuivre', 100)
 ;
